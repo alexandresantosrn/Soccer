a917f6b3053834bdebe34964691dd41c8e784256
marlusmarcos <marlusufrn@gmail.com>
2023-07-10 21:56:38 -0300

    add view bubscando pelo id

--- Soccer/src/main/java/dao/ClassificacaoDAO.java
+++ Soccer/src/main/java/dao/ClassificacaoDAO.java
@@ -12,7 +12,33 @@ import model.Classificacao;
 public class ClassificacaoDAO {
 	
 	public static List<Classificacao> getClassificacao() {
-		String sql = "select * from Classificacao";
+		//String sql = "select * from Classificacao";
+		int campeonatoId = 1; // Substitua pelo ID do campeonato desejado
+
+		String sql = "SELECT t.idTime, t.nome AS Time, " +
+	             "COALESCE(SUM(CASE WHEN dp.Time_idTimeCasa = t.idTime THEN " +
+	             "CASE WHEN dp.golsCasa > dp.golsVisitante THEN 3 " +
+	             "WHEN dp.golsCasa = dp.golsVisitante THEN 1 " +
+	             "ELSE 0 END " +
+	             "ELSE " +
+	             "CASE WHEN dp.golsCasa < dp.golsVisitante THEN 3 " +
+	             "WHEN dp.golsCasa = dp.golsVisitante THEN 1 " +
+	             "ELSE 0 END " +
+	             "END), 0) AS Pontos, " +
+	             "COALESCE(SUM(CASE WHEN dp.Time_idTimeCasa = t.idTime THEN dp.golsCasa ELSE dp.golsVisitante END), 0) AS GolsMarcados, " +
+	             "COALESCE(SUM(CASE WHEN dp.Time_idTimeCasa = t.idTime THEN dp.golsVisitante ELSE dp.golsCasa END), 0) AS GolsSofridos " +
+	             "FROM Time t " +
+	             "LEFT JOIN Detalhes_Partida dp ON dp.Time_idTimeCasa = t.idTime OR dp.Time_idTimeVisitante = t.idTime " +
+	             "LEFT JOIN Partida p ON dp.Partida_idPartida = p.idPartida " +
+	             "LEFT JOIN Rodada r ON p.Rodada_idRodada = r.idRodada " +
+	             "LEFT JOIN Campeonato c ON r.Campeonato_idCampeonato = c.idCampeonato " +
+	             "WHERE c.idCampeonato = "+campeonatoId+ " "+
+	             "GROUP BY t.idTime, t.nome " +
+	             "ORDER BY Pontos DESC, GolsMarcados DESC";
+
+		 
+		
+		
 		List<Classificacao> classificacao = new ArrayList();
 		try {
 
